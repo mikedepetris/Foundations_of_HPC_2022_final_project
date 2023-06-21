d4da2a39e5f056de297365e404d9398f904cd601
Mike De Petris <mikedepetris@gmail.com>
2023-06-21 12:16:51 +0200

    scale_omp.sh updated to pass size as a parameter

--- scale_omp.sh
+++ scale_omp.sh
@@ -23,6 +23,11 @@ if [ $# == 2 ]; then
   TYPE="$1"
   SNAPAT="$2"
 fi
+if [ $# == 3 ]; then
+  TYPE="$1"
+  SNAPAT="$2"
+  SIZE="$3"
+fi
 echo "Selected type of execution: $TYPE"
 
 module load architecture/AMD
@@ -44,17 +49,18 @@ echo "action,world_size,number_of_steps,number_of_steps_between_file_dumps,mpi_s
 
 echo OMP scalability begin
 
-for REP in {1..5}; do
-  for SIZE in 10000 1000 100; do
+for REP in {1..10}; do
+#  for SIZE in 10000; do
 
     if [ "$TYPE" == i ]; then
+#SIZE=10000
       for threads in {1..64}; do
         echo rep $REP scalability -i "$SIZE" "$threads"
         export OMP_NUM_THREADS=$threads
         mpirun -n 1 --map-by socket gameoflife.x -i -k $SIZE -f pattern_random$SIZE -q >>"$csvname"
       done
     else
-      for threads in {1..64}; do
+      for threads in {64..1}; do
         echo rep $REP scalability -e"$TYPE" "$SIZE" "$threads"
         export OMP_NUM_THREADS=$threads
         {
@@ -67,7 +73,7 @@ for REP in {1..5}; do
       done
     fi
 
-  done
+#  done
 done
 
 echo scalability end
