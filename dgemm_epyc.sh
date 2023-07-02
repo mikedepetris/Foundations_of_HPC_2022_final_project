f088d9a376e716d1912fe67e379490050c325726
Mike De Petris <mikedepetris@gmail.com>
2023-07-02 16:16:48 +0200

    sh edit

--- dgemm_epyc.sh
+++ dgemm_epyc.sh
@@ -33,15 +33,15 @@ for LIB in oblas oblas_optimized mkl mkl_optimized; do
   now=$(date +"%Y-%m-%d_%H-%M-%S")
   csvname=dgemm_"$LIB"_epyc_$(hostname)_$now.csv
   #echo "$csvname $(hostname) $now"
-  echo "$csvname"
+  #echo "$csvname"
   #2000,2000,2000,0.034462963,0.034463,464.266523
   echo "m,n,k,elapsed1,elapsed2,GFLOPS" >"$csvname"
   #srun -n1 --cpus-per-task=64 ./dgemm_oblas.x 2000 2000 200>discarded_first_result_"$csvname"
   #echo "$csvname $(hostname) $now">"$csvname"
-  for REP in {1..10}; do
+  for REP in {1..5}; do
     #increase the size of matrices size from 2000x2000 to 20000x20000 (single precision) and analyse the scaling of the GEMM calculation for at least MKL and openblas.
-    for SIZE in {2000..20000..500}; do
-      echo rep "$REP" size "$SIZE" srun -n1 --cpus-per-task=64 ./dgemm_$LIB.x "$SIZE" "$SIZE" "$SIZE"'>>'"$csvname"
+    for SIZE in {30000..20000..500}; do
+      echo "$csvname","$csvname","$(hostname)",rep "$REP" size "$SIZE" srun -n1 --cpus-per-task=64 ./dgemm_$LIB.x "$SIZE" "$SIZE" "$SIZE"'>>'"$csvname"
       srun -n1 --cpus-per-task=64 ./dgemm_$LIB.x "$SIZE" "$SIZE" "$SIZE" >>"$csvname"
       #srun -n1 --cpus-per-task=64 ./dgemm_oblas.x 2000 2000 2000
     done
